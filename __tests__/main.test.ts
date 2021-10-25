5d48bf8d18c52f3f5352355128bc064988263a77
Nikhil <sainikhilsharma@gmail.com>
2021-10-25 17:04:18 +0530

    fixed lint issues

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,45 +1,14 @@
 import { DateType } from '../src/constants';
 import dateToJDN from './../src/julianConvert';
 describe('JulianConvert', () => {
-  // const name = 'John';
-  // let hello: string;
-
-  // let timeoutSpy: jest.SpyInstance;
-
-  // // Act before assertions
-  // beforeAll(async () => {
-  //   // Read more about fake timers
-  //   // http://facebook.github.io/jest/docs/en/timer-mocks.html#content
-  //   // Jest 27 now uses "modern" implementation of fake timers
-  //   // https://jestjs.io/blog/2021/05/25/jest-27#flipping-defaults
-  //   // https://github.com/facebook/jest/pull/5171
-  //   jest.useFakeTimers();
-  //   timeoutSpy = jest.spyOn(global, 'setTimeout');
-
-  //   const p: Promise<string> = greeter(name);
-  //   jest.runOnlyPendingTimers();
-  //   hello = await p;
-  // });
-
-  // // Teardown (cleanup) after assertions
-  // afterAll(() => {
-  //   timeoutSpy.mockRestore();
-  // });
-  beforeAll(async()=>{
-
-  })
-  afterAll(()=>{
-
-  })
-
   // Assert if setTimeout was called properly
   it('should return JDN with default parameters', () => {
     expect(dateToJDN()).toBeGreaterThanOrEqual(2459513);
   });
-  it('should return JDN with given date',()=>{
-    expect(dateToJDN(new Date(1672,0,1))).toBe(2331746)
-  })
-  it('should return JDN with given date and type',()=>{
-    expect(dateToJDN(new Date(1200,0,1),DateType.JULIAN)).toBe(2159358);
-  })
+  it('should return JDN with given date', () => {
+    expect(dateToJDN(new Date(1672, 0, 1))).toBe(2331746);
+  });
+  it('should return JDN with given date and type', () => {
+    expect(dateToJDN(new Date(1200, 0, 1), DateType.JULIAN)).toBe(2159358);
+  });
 });
--- src/julianConvert.ts
+++ src/julianConvert.ts
@@ -1,34 +1,32 @@
-import { DateType } from "./constants";
+import { DateType } from './constants';
 
-function dateToJDN(greg: Date = new Date(),dateType:DateType=DateType.GREG): Number {
+function dateToJDN(
+  greg: Date = new Date(),
+  dateType: DateType = DateType.GREG,
+): number {
   const [gYear, gMonth, gDay] = [
     greg.getFullYear(),
-    greg.getMonth()+1,
+    greg.getMonth() + 1,
     greg.getDate(),
   ];
-  let tempMonth: number,
-    tempYear: number,
-    totalDays: number,
-    totalYears: number,
-    jdn: number,
-    offsetDays:number;
+  let tempMonth: number, totalYears: number, offsetDays: number;
   tempMonth = Math.floor((14 - gMonth) / 12);
-  tempYear = gYear + 4800 - tempMonth;
+  const tempYear = gYear + 4800 - tempMonth;
   tempMonth = gMonth + 12 * tempMonth - 3;
-  totalDays = Math.floor((153 * tempMonth + 2) / 5);
-  switch(dateType){
+  const totalDays = Math.floor((153 * tempMonth + 2) / 5);
+  switch (dateType) {
     case DateType.GREG:
-    totalYears =
-    Math.floor(tempYear / 4) -
-    Math.floor(tempYear / 100) +
-    Math.floor(tempYear / 400);
-    offsetDays = 32045;
-    break;
+      totalYears =
+        Math.floor(tempYear / 4) -
+        Math.floor(tempYear / 100) +
+        Math.floor(tempYear / 400);
+      offsetDays = 32045;
+      break;
     case DateType.JULIAN:
-        totalYears = Math.floor(tempYear/4);
-        offsetDays = 32083;
+      totalYears = Math.floor(tempYear / 4);
+      offsetDays = 32083;
   }
-  jdn = gDay + totalDays + 365 * tempYear + totalYears - offsetDays;
+  const jdn = gDay + totalDays + 365 * tempYear + totalYears - offsetDays;
   return jdn;
 }
 export default dateToJDN;
